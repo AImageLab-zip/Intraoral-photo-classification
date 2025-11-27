c76d1020b8f0267f42b45ef208649e1ae8343b61
Yibe1616 <yibebdu21@gmail.com>
2025-11-27 11:19:24 +0100

    third commit

--- config.py
+++ config.py
@@ -1,31 +1,18 @@
-# ------------------------------------------------------------
-# config.py (FINAL CLEAN VERSION)
-# ------------------------------------------------------------
 
 import torch
-
-# ==== Classes ====
 CLASSES = ["center", "down", "left", "right", "up"]
 
-# ==== DataLoader settings ====
 BATCH_SIZE = 8
 NUM_WORKERS = 4
 TEST_SIZE = 0.2
 RANDOM_STATE = 42
-
-# ==== Training hyperparameters ====
 NUM_EPOCHS = 15
 LEARNING_RATE = 1e-4
 WEIGHT_DECAY = 1e-5
-
-# ==== Image normalization ====
 IMAGENET_MEAN = [0.485, 0.456, 0.406]
 IMAGENET_STD  = [0.229, 0.224, 0.225]
-
-# ==== Device ====
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
-# ==== WandB ====
-WANDB_PROJECT = "Tooth_Classification1"
+WANDB_PROJECT = "Intraoral-photo-classification"
 
 print(f"Config loaded | Device={DEVICE}")
